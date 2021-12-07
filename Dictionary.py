1ad28faa376017119e219844768b12fa7bdfe94e
Akanimo <Akanumoh4@gmail.com>
2021-12-07 14:58:00 +0100

    Nested Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,8 +1,9 @@
 person = {"dict1":{"name":"udeme"},"dict2":{"age":12},"dict3":{"complexion":"dark"}}
 for key, value in person.items():
-    """accessing the nested dictionary"""
+    """accessing the key in the nested dictionary"""
     print (key)
     for key in value.keys():
+        """accessing the key in the values"""
 	    print (key)
     for value in value.values():
         print (value)
